3a2fe723892e7be9772f133de934c7ca52f75af8
florian <b.florian91@icloud.com>
2019-11-09 05:10:33 +0100

    nice scroll view works

--- src/App.vue
+++ src/App.vue
@@ -1,5 +1,5 @@
 <template>
-  <div id="app">
+  <div id="home">
     <NavBar />
     <Home />
     <NewsLetter />
--- src/components/NavBar.vue
+++ src/components/NavBar.vue
@@ -1,29 +1,24 @@
 <template>
   <nav class="navbar navbar-expand-md navbar-light fixed-top py-4" id="main-nav">
     <div class="container">
-      <a href="#home" class="navbar-brand">
-        <img src="../../src/assets/img/mlogo.png" width="50" height="50" alt="">
+      <a href="home" class="navbar-brand">
+        <img src="../../src/assets/img/mlogo.png" width="50" height="50" alt="logo">
         <h3 class="d-inline align-middle">Mizuxe</h3>
       </a>
       <button class="navbar-toggler" data-toggle="collapse" data-target="#navbarCollapse">
         <span class="navbar-toggler-icon"></span>
       </button>
-      <div class="collapse navbar-collapse" id="navbarCollapse">
-        <ul class="navbar-nav ml-auto">
-          <li class="nav-item">
-            <a href="#home" class="nav-link">Home</a>
-          </li>
-          <li class="nav-item">
-            <a href="#about" class="nav-link">About</a>
-          </li>
-          <li class="nav-item">
-            <a href="#authors" class="nav-link">Meet The Authors</a>
-          </li>
-          <li class="nav-item">
-            <a href="#contact" class="nav-link">Contact</a>
-          </li>
-        </ul>
-      </div>
+                <div class="collapse navbar-collapse" id="navbarCollapse">
+            <ul class="navbar-nav ml-auto">
+              <li class="nav-item" v-for="(value, name, index) in items" :key="index">
+                <span
+                  @click="selectItem(value)"
+                  class="nav-link"
+                  style="cursor: pointer;"
+                >{{ name }}</span>
+              </li>
+            </ul>
+          </div>
     </div>
   </nav>
 </template>
@@ -33,12 +28,20 @@ export default {
   data() {
     return {
       items: {
-        Home: "home",
-        About: "about",
-        "Meet the Author": "authors",
-        Contact: "contact"
+        Home: "#home",
+        About: "#about",
+        "Meet the Author": "#authors",
+        Contact: "#contact"
       }
-    };
+    }
+  },
+  methods: {
+    selectItem(item) {
+      if (item === "#home")
+        scrollTo({top: 0, behavior: "smooth"})
+      else 
+        document.querySelector(item).scrollIntoView({ behavior: "smooth", block: "center"})
+    }
   }
 };
 </script>
